735a50ec4c685ce5453aa8be6521835f0b106051
JGutierrez1695 <74220705+JGutierrez1695@users.noreply.github.com>
2021-03-05 12:35:40 -0700

    Add files via upload
    My first assignment

---
+++ LashesShopingCarts/LashesShoppingCart/LashesShoppingCart/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LashesShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theActualResult = MyMethod();
+
+            Console.WriteLine(theActualResult);
+        }
+
+        public static int MyMethod()
+        {
+            int result = 30;
+
+            return result;
+        }
+    }
+}

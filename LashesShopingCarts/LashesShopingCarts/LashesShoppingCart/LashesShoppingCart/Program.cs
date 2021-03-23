6f3fe2f27be964ce8c08baabdc9c4300d520839a
JGutierrez1695 <74220705+JGutierrez1695@users.noreply.github.com>
2021-03-22 22:09:15 -0600

    Add files via upload

---
+++ LashesShopingCarts/LashesShopingCarts/LashesShoppingCart/LashesShoppingCart/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LashesShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int integerVariable = 0;
+
+            Lashes lashesObject = new Lashes();
+            lashesObject.style = "Queen lashes";
+
+            Lashes lashesObject2 = new Lashes();
+            lashesObject2.style = "Doll lashes";
+
+            Console.WriteLine($"Add {lashesObject2.style} and {lashesObject .style} to my shopping cart");
+        }
+           
+     }
+
+     public class Lashes
+    {
+
+       public string style { get; set; }
+
+    }
+  
+
+
+ }
+
